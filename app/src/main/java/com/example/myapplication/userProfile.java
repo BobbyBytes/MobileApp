d6b9ddad38f2bdba1175fa81118204ce04331eb8
Max-Rider <50746197+Max-Rider@users.noreply.github.com>
2019-11-11 17:29:34 -0500

    Created a link view kinda thing for posting links to youtube videos. I also rearranged some of the view and removed the layout title

--- app/src/main/java/com/example/myapplication/userProfile.java
+++ app/src/main/java/com/example/myapplication/userProfile.java
@@ -31,6 +31,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.webkit.MimeTypeMap;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -53,22 +54,6 @@ public class userProfile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        YouTubePlayerFragment youtubeFragment = (YouTubePlayerFragment)
-                getFragmentManager().findFragmentById(R.id.youtubeFragment);
-        youtubeFragment.initialize("AIzaSyBBYu_iE7KoEVExaB59aUxJ-0fcK5S7CnQ", new YouTubePlayer.OnInitializedListener() {
-            @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider,
-                                                YouTubePlayer youTubePlayer, boolean b) {
-                // do any work here to cue video, play video, etc.
-                youTubePlayer.cueVideo("5xVh-7ywKpE");
-            }
-            @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider,
-                                                YouTubeInitializationResult youTubeInitializationResult) {
-
-            }
-        });
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
